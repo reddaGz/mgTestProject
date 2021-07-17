38d033d348a14000c534e6fbe28345823e4c6ac0
frekassa2101 <69208220+frekassa2101@users.noreply.github.com>
2021-07-17 00:59:50 -0400

    test2

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-function add(x,y){
-    return x+y;
+function add(x, y) {
+    return x + y;
 }
-function sub(x,y){
-    return x-y;
+function sub(x, y) {
+    return x - y;
 }
+
+/////
+function myEven(arr) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+            result.push(arr[i]);
+        }
+    }
+    return result;
+}
+let myArr = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(myEven(myArr));
